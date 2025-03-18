f229ccb106fab3e8a0da53f90eb46ddc5a6b3d1c
Him-anshuSharma <sharmanshu0103@gmail.com>
2025-03-18 13:18:35 +0530

    fix: firestore error handling

--- timetable/controllers/db_controller.py
+++ timetable/controllers/db_controller.py
@@ -1,7 +1,12 @@
 from init import db
+from fastapi import HTTPException
 from timetable.models.timetable import Timetable
+from firebase_admin.exceptions import FirebaseError
 
 def save_timetable(timetable:Timetable,id:str):
-    ref = db.collection(str).document('timetable')
-    ref.set(timetable.get_day_schedule)
-    return True
+    try:
+        ref = db.collection(str).document('timetable')
+        ref.set(timetable.get_day_schedule)
+        return 
+    except FirebaseError as e:
+        return HTTPException(status_code=500,detail=str(e))
