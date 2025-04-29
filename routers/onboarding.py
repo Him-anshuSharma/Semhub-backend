d2cdf31555691dfb0f7f9d79d631ec08b860aa02
Him-anshuSharma <sharmanshu0103@gmail.com>
2025-04-29 21:50:44 +0530

    feat: controller and router

--- routers/onboarding.py
+++ routers/onboarding.py
@@ -1,8 +1,6 @@
 import os
 from fastapi import APIRouter, UploadFile, File
-from init import client
-import json
-from constants import onboarding_prompt as prompt, gemini_model
+from services.onboarding import makeprofile
 from models.onboarding import Response
 
 
@@ -13,34 +11,6 @@ async def makeprofile(
     audios: list[UploadFile] = None,
     images : list[UploadFile] = File(...)
 ):
-    DIR = "tempfiles"
-    os.makedirs(DIR,exist_ok=True)
-    file_locations = []
-
-    if audios is not None:
-        #TODO
-        print("have audio files")
-
-    for image in images:
-        image_path = os.path.join(DIR,image.filename)
-        with open(image_path,"wb") as out_file:
-            content = await image.read()
-            out_file.write(content)
-        file_locations.append(image_path)
-
-    contents = [prompt]
-
-    for file in file_locations:
-        myfile = client.files.upload(file = file)
-        contents.append(myfile)
-
-    response = client.models.generate_content(
-        model = gemini_model,
-        contents=contents
-    )
-    print("gemini response", response.text)
-    json_data= json.loads(response.text)
-    gemini_response = Response.model_validate(json_data)
-    print(gemini_response.goals)
-    return gemini_response
+    tasks_goals = makeprofile(audios, images)
+    return tasks_goals
 
---
+++ services/onboarding.py
@@ -0,0 +1,41 @@
+
+import os
+from init import client
+from fastapi import UploadFile, File
+import json
+from constants import onboarding_prompt as prompt, gemini_model
+from models.onboarding import Response
+
+async def makeprofile( 
+        audios: list[UploadFile] = None,
+        images : list[UploadFile] = File(...)
+        ):
+    DIR = "tempfiles"
+    os.makedirs(DIR,exist_ok=True)
+    file_locations = []
+
+    if audios is not None:
+        #TODO
+        print("have audio files")
+
+    for image in images:
+        image_path = os.path.join(DIR,image.filename)
+        with open(image_path,"wb") as out_file:
+            content = await image.read()
+            out_file.write(content)
+        file_locations.append(image_path)
+
+    contents = [prompt]
+
+    for file in file_locations:
+        myfile = client.files.upload(file = file)
+        contents.append(myfile)
+
+    response = client.models.generate_content(
+        model = gemini_model,
+        contents=contents
+    )
+    print("gemini response", response.text)
+    json_data= json.loads(response.text)
+    gemini_response = Response.model_validate(json_data)
+    return gemini_response
